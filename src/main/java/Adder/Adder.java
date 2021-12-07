906ee729f9d9a237e66d4e6a66d4014c9e8215cc
Daniel Casique <daniel.a.casique.-nd@disney.com>
2021-12-07 15:24:25 -0500

    fixing packages name, adding missing exercises

---
+++ src/main/java/Adder/Adder.java
@@ -0,0 +1,17 @@
+package Adder;
+
+/**
+ * The goal of this exercise is to understand how to use and implement an abstract class
+ */
+
+abstract class Calculator {
+    abstract int add(int a, int b);
+}
+
+public class Adder extends Calculator{
+
+    @Override
+    int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/StringsCollection/StringCollectionSolution.java
@@ -0,0 +1,83 @@
+package StringsCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/*
+ * Create the StringsCollection class here.
+ */
+
+class StringsCollection {
+
+    private List<String> stringsCollection;
+
+    public StringsCollection() {
+        this.stringsCollection = new ArrayList<>();
+    }
+
+    public synchronized void addString(String string){
+        stringsCollection.add(string);
+    }
+
+    public List<String> getStringsCollection(){
+        return stringsCollection;
+    }
+}
+
+class StringsCollectionRunnable implements Runnable {
+    private final StringsCollection stringsCollection;
+    private final int stringsCount;
+    private final String threadName;
+
+    public StringsCollectionRunnable(StringsCollection stringsCollection, int stringsCount, String threadName) {
+        this.stringsCollection = stringsCollection;
+        this.stringsCount = stringsCount;
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        for (int j = 0; j < stringsCount; j++) {
+            this.stringsCollection.addString(threadName + String.valueOf(j + 1));
+        }
+    }
+}
+
+public class StringCollectionSolution {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final StringsCollection STRINGS_COLLECTION = new StringsCollection();
+
+    public static void main(String[] args) {
+        int threadsCount = Integer.parseInt(SCANNER.nextLine());
+        Thread[] threads = new Thread[threadsCount];
+
+        for (int i = 0; i < threadsCount; i++) {
+            int stringsCount = Integer.parseInt(SCANNER.nextLine());
+
+            threads[i] = new Thread(new StringsCollectionRunnable(STRINGS_COLLECTION, stringsCount, String.valueOf(i + 1)));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threadsCount; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException ex) {
+                System.err.println(ex);
+            }
+        }
+
+        List<String> stringsCollection = STRINGS_COLLECTION.getStringsCollection();
+        System.out.println(stringsCollection.size());
+
+        int nonNullStrings = 0;
+        for (String string: stringsCollection) {
+            if (string != null) {
+                nonNullStrings++;
+            }
+        }
+
+        System.out.println(nonNullStrings);
+    }
+}
