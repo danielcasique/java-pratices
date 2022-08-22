14903b0aa1ed47006ef7056e5300efd38fc962a1
Daniel Casique <daniel.casique@gmail.com>
2022-08-22 09:50:19 -0500

    adding a practice about set in the rush hour exercise

--- src/main/java/RushHour/RushHour.java
+++ src/main/java/RushHour/RushHour.java
@@ -3,6 +3,7 @@ package RushHour;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RushHour {
@@ -11,6 +12,32 @@ public class RushHour {
      * Posible list of values for the list input
      *  2022-01-01T00:00:01
      */
+
+    /*
+    private List<DepositEntry> getDepositsByCreditCardAndCisPeriod(List<DepositEntry> succeededDeposits,
+                                                                   List<RequisitesItem> requisites) {
+
+        //Map <String> creditCardRequisites =	getCreditCardRequisites(requisites).collect(Collectors.toMap(x, Function.identity()));
+
+        Set<String> creditCardRequisitesSet = getCreditCardRequisites(requisites);
+        return succeededDeposits.stream()
+                .filter(item -> item.getCreated().isAfter(LocalDateTime.now().minusDays(30)))
+                .filter(item -> Objects.nonNull(creditCardRequisitesSet.contains(item.getRequisiteId())))
+                .collect(Collectors.toList());
+        //creditCardRequisites.contains(item.getRequisiteId())
+    }
+
+    private Set <String> getCreditCardRequisites(List<RequisitesItem> requisites) {
+        return requisites.stream()
+                .filter(requisite -> CREDIT_CARD.equalsIgnoreCase(requisite.getMethod()))
+                .map(RequisitesItem::getId)
+                .collect(Collectors.toSet());
+    }*/
+    /**
+    succededDeposits : 100 items
+    requisites: 50 items
+    Iterations: 100 * 50 = 5000   => 150
+     **/
 
     public static String getRushHour(List<String> list){
         Optional<Map.Entry<String, Long>> result = list.parallelStream().map(x -> x.substring(11,13))
