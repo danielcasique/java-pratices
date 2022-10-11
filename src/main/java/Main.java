85213c783f322be483276a73a98191a6a365b6dc
Daniel Casique <daniel.a.casique.-nd@disney.com>
2022-10-11 18:56:23 -0500

    adding immutable examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,30 @@
 package com.casique;
 
+import java.lang.reflect.Method;
+
+class Test{
+    private void message(){
+        System.out.printf("hello java");
+    }
+}
+
 public class Main {
 
+    public void messageTest(){
+       // System.out.printf(String.format("Runtime.freeMemory()=%d", Runtime.freeMemory()));
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Class c = null;
+        try {
+            c = Class.forName("com.casique.Test");
+            Object o = c.newInstance();
+            Method m = c.getDeclaredMethod("message", null);
+            m.setAccessible(true);
+            m.invoke(o, null);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
