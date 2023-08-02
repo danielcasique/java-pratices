b7e421aaaaaf0ed06dc2c769e6bd97258c36fb4f
Daniel Casique Caceres <daniel.a.casique.-nd@disney.com>
2023-08-01 21:33:28 -0500

    adding RxJava examples

--- src/main/java/org/casique/rxjava/RxExample2.java
+++ src/main/java/org/casique/rxjava/RxExample2.java
@@ -2,19 +2,18 @@ package org.casique.rxjava;
 
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.function.Consumer;
 
-class Test {
+class Test2 {
 
   private List<String> list;
 
-  public static Test newInstance() {
+  public static Test2 newInstance() {
     System.out.println("newInstance : " + Thread.currentThread().getName());
-    Test test = new Test();
+    Test2 test = new Test2();
     test.list = List.of("A", "B", "C", "D", "E", "F", "G", "H");
     return test;
   }
@@ -31,30 +30,28 @@ class Test {
     return this.list.stream().filter(x -> x.equals(value)).findFirst();
   }
 }
-
-public class RxExample {
+public class RxExample2 {
 
   public static void main(String[] args) throws InterruptedException {
     Consumer<Optional<String>> consume = optionalValue -> {
-      System.out.println("Consumer : " +  optionalValue + " : "  + Thread.currentThread().getName());
+      System.out.println("consumer : " + Thread.currentThread().getName());
       if(optionalValue.isPresent()){
         System.out.println(optionalValue.get());
       }else {
         System.out.println("Item not found");
       }
     };
-    Test test = Test.newInstance();
+    Test2 test = Test2.newInstance();
     Flowable.fromOptional(test.getList())
         .observeOn(Schedulers.io())
         .flatMap(Flowable::fromIterable)
-        .parallel()
-        .runOn(Schedulers.io())
         .map(test::getItem)
         .doOnNext(consume::accept)
-        .sequential()
+        .doOnError(error -> System.out.println(error.getMessage()))
         .doOnComplete(() -> System.out.println("finish!"))
         .blockingSubscribe();
 
     Thread.sleep(8000);
   }
+
 }
